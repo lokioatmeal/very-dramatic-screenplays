1040871a3e9f9e99f2a98f1e218dca9e8476d0d5
Chris G <chrisg@armorclass.com>
2026-01-19 16:27:30 -0800

    Reorganize structure: move index.html to src, docs to docs folder, remove public

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
   base: '/very-dramatic-screenplays/',
   build: {
-    outDir: 'dist',
+    outDir: '../dist',
   },
 });
